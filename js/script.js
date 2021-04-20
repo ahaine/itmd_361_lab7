422c8ba297f406b311d620b4a768b4a516b17188
ahaine <77943398+ahaine@users.noreply.github.com>
2021-04-20 18:32:49 -0400

    Fixed java script and now it works properly.
    Had to fix some errors with my java script.  Just ended up being some typos.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
-//add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
+	var inputtext = document.getElementById('entryinput');
+	var b1 = document.getElementById('entrybutton');
+	var output1 = document.getElementById('textoutput');
+	
+	function whatHappensEvent(){
+		alert('Andrew Haines: ' + inputtext.value);
+		output1.innerHTML = inputtext.value;
+	}
+	b1.addEventListener('click', whatHappensEvent);
+}
 window.addEventListener('load', init);
